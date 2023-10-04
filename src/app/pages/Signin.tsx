b4b1357795eb419cf7d5dcebad5e3eebb3c55fd7
Mümin Öztürk <85707419+ozturkmumin@users.noreply.github.com>
2023-10-05 00:09:03 +0300

    fixed

--- src/app/pages/Signin.tsx
+++ src/app/pages/Signin.tsx
@@ -1,41 +1,8 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
-import { signIn } from "next-auth/react";
 const submitEvent = new Event("submit") as Event;
 
 const Signin = () => {
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleSubmit = async (e: Event) => {
-    e.preventDefault();
-    // Inputların doluluk durumunu kontrol edin
-    if (!credentials.username || !credentials.password) {
-      alert("Kullanıcı adı ve şifre gereklidir.");
-      return;
-    }
-    const result = await signIn("credentials", {
-      username: credentials.username,
-      password: credentials.password,
-      redirect: false,
-    });
-    if (result?.error) {
-      console.error("Giriş hatası:", result.error);
-    } else {
-      console.log("Giriş başarılı!");
-    }
-  };
-  const sahteKullaniciBilgileri = {
-    username: "testuser",
-    password: "testpassword",
-  };
-  const testGirisYap = () => {
-    setCredentials(sahteKullaniciBilgileri);
-    handleSubmit(new Event("submit") as Event);
-  };
   return (
     <div className="container">
       <div className="w-100 h-screen flex justify-center items-center">
@@ -48,13 +15,6 @@ const Signin = () => {
                   type="text"
                   className="shadow-2xl w-full p-3 rounded-lg"
                   placeholder="Username"
-                  onChange={(e) =>
-                    setCredentials({
-                      ...credentials,
-                      username: e.target.value,
-                    })
-                  }
-                  autoComplete="off"
                 />
               </div>
               <div>
@@ -62,23 +22,11 @@ const Signin = () => {
                   type="password"
                   className="shadow-2xl w-full p-3 rounded-lg"
                   placeholder="Password"
-                  onChange={(e) =>
-                    setCredentials({
-                      ...credentials,
-                      password: e.target.value,
-                    })
-                  }
-                  autoComplete="off"
                 />
               </div>
             </div>
             <div className="card-actions justify-center pt-3 pb-1">
-              <button
-                className="btn w-full btn-info text-white"
-                onClick={testGirisYap}
-              >
-                Login
-              </button>
+              <button className="btn w-full btn-info text-white">Login</button>
             </div>
             <div className="flex text-xs">
               Create a new account if you don't have one{" "}
